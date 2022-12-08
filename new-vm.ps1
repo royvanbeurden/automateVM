7a69b46ae8f9c331a0b4ba39450745c9c601c2cb
royvanbeurden <114396197+royvanbeurden@users.noreply.github.com>
2022-12-08 10:23:26 +0100

    Create new-vm.ps1

---
+++ new-vm.ps1
@@ -0,0 +1,23 @@
+$resourceGoupName = 'webserverrsg'
+$azureRegion = 'West Europe'
+$vmName = 'webservervm'
+
+#Create the resource group
+New-AzResourceGroup -Name $resourceGoupName -Location $azureRegion
+
+New-AzVm `
+    -ResourceGroupName = $resourceGroupName `
+    -Name = $vmName `
+    -Location = $azureRegion `
+    -Image = UbuntuTLS `
+    -size = Standard_B2s `
+    -PublicIpAddressName = myPubIP `
+    -OpenPorts = 80 `
+    -GenerateSshKey `
+    -SshKeyName = mySSHKey
+    
+Invoke-AzVMRunCommand `
+   -ResourceGroupName = $resourceGroupName `
+   -Name = $vmName `
+   -CommandId 'RunShellScript' `
+   -ScriptString 'sudo apt-get update && sudo apt-get install -y nginx'
